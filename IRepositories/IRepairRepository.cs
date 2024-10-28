f478ceabdfec24be5b546badd5eb4df1672c38ac
George Tolios <toliosg2pr@gmail.com>
2024-10-29 00:18:49 +0200

    Add Repair Repository

---
+++ IRepositories/IRepairRepository.cs
@@ -0,0 +1,20 @@
+﻿using Technico.Models;
+
+namespace Technico.IRepositories;
+
+public interface IRepairRepository
+{
+    public Task<List<Repair>> GetRepairs();
+
+    public Task<Repair?> GetRepair(int id);
+    
+    public Task<bool> RepairExists(int id);
+
+    Task<bool> CreateRepair(Repair repair, Owner owner);
+  
+    Task<bool> UpdateRepair( Repair repair);
+    
+    Task<bool> DeleteRepair(Repair repair);
+    
+    Task<bool> Save();
+}
---
+++ Repositories/RepairRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Technico.Data;
+using Technico.IRepositories;
+using Technico.Models;
+
+namespace Technico.Repositories;
+
+public class RepairRepository: IRepairRepository
+{ private readonly TechnicoDbContext _context;
+    private readonly IOwnerRepository _ownerRepository;
+
+    public RepairRepository(TechnicoDbContext context, IOwnerRepository ownerRepository)
+    {
+        _context = context;
+        _ownerRepository = ownerRepository;
+    }
+    
+    public async Task<List<Repair>> GetRepairs()
+    {
+        return await _context.Repairs.OrderBy(r => r.DateTime).ToListAsync();
+    }
+
+    public async Task<Repair?> GetRepair(int id)
+    {
+        return await _context.Repairs.Where(r => r.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<bool> RepairExists(int id)
+    {
+        return await _context.Repairs.AnyAsync(r => r.Id == id);
+    }
+
+    public async Task<bool> CreateRepair(Repair repair, Owner owner)
+    {
+        var ownerExists = await _ownerRepository.OwnerExists(owner.VatNumber);
+        if (!ownerExists)
+        {
+            return false;
+        }
+
+        repair.Owner = owner;
+        _context.Add(repair);
+
+        return await Save();
+    }
+
+    public async Task<bool> UpdateRepair(Repair repair)
+    {
+        _context.Update(repair);
+        return await Save();
+    }
+
+    public Task<bool> DeleteRepair(Repair repair)
+    {
+        _context.Remove(repair);
+        return Save();
+    }
+
+    public async Task<bool> Save()
+    {
+        var saved = _context.SaveChangesAsync();
+        return await saved > 0;
+    }
+}
