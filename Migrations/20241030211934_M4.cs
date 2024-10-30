ba949c29a1b1f57cc8257c5585056be8fa07a02e
George Tolios <toliosg2pr@gmail.com>
2024-10-30 23:24:15 +0200

    Add new Migration

---
+++ Migrations/20241030211934_M4.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Technico.Migrations
+{
+    /// <inheritdoc />
+    public partial class M4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Repairs_Owners_OwnerId",
+                table: "Repairs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OwnerId",
+                table: "Repairs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Repairs_Owners_OwnerId",
+                table: "Repairs",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Repairs_Owners_OwnerId",
+                table: "Repairs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OwnerId",
+                table: "Repairs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Repairs_Owners_OwnerId",
+                table: "Repairs",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
